d62a35806edc9d350ceebf87c78ab18882979b7b
Eduardo Caitano <83654560+Duduzyn-py@users.noreply.github.com>
2024-09-25 19:36:40 -0300

    Create aula_25_09.cs

---
+++ aulas_cs/aula_25_09.cs
@@ -0,0 +1,43 @@
+string[] nomes = new string[5];
+double[] notas = new double[5];
+
+Console.WriteLine("Digite o nome do primeiro alunos: ");
+nomes[0] = Console.ReadLine();
+
+Console.WriteLine("Digite a nota do aluno:");
+notas[0] = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o nome do segundo alunos: ");
+nomes[1] = Console.ReadLine();
+
+Console.WriteLine("Digite a nota do aluno:");
+notas[1] = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o nome do terceiro alunos: ");
+nomes[2] = Console.ReadLine();
+
+Console.WriteLine("Digite a nota do aluno:");
+notas[2] = double.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Digite o nome do quarto alunos: ");
+nomes[3] = Console.ReadLine();
+
+Console.WriteLine("Digite a nota do aluno:");
+notas[3] = double.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Digite o nome do quinto alunos: ");
+nomes[4] = Console.ReadLine();
+
+Console.WriteLine("Digite a nota do aluno:");
+notas[4] = double.Parse(Console.ReadLine());
+
+Console.WriteLine("NOTAS DOS ALUNOS");
+Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=");
+Console.WriteLine($"{nomes[0]}: {notas[0]} ");
+Console.WriteLine($"{nomes[1]}: {notas[1]} ");
+Console.WriteLine($"{nomes[2]}: {notas[2]} ");
+Console.WriteLine($"{nomes[3]}: {notas[3]} ");
+Console.WriteLine($"{nomes[4]}: {notas[4]} ");
+
