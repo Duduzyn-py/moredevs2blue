c9f406f0e4ca3ad16e5f9a4b9b8247a2bee8716f
Eduardo Caitano <83654560+Duduzyn-py@users.noreply.github.com>
2024-09-30 21:02:34 -0300

    Update and rename aula30_09.cs to aula_30_09.cs

--- aulas_cs/aula_30_09.cs
+++ aulas_cs/aula_30_09.cs
@@ -26,3 +26,30 @@ Console.WriteLine("Fime de execução");
 Console.WriteLine($"Maior número: {maiorNumero}");
 Console.WriteLine($"Menor número: {menorNumero}");
 */
+//DO-WHILE
+string entrada = "OK";
+int maiorNumero = int.MinValue;
+int menorNumero = int.MaxValue;
+
+do
+{
+    Console.WriteLine("Digite um numero ou OK para sair");
+    entrada = Console.ReadLine();
+
+    if (int.TryParse(entrada, out int numero))
+    { 
+        if (menorNumero > numero)
+        {
+            menorNumero = numero;
+        }
+
+        if (maiorNumero < numero)
+        {
+            maiorNumero = numero;
+        }
+    }
+    else if (entrada.ToUpper() != "OK")
+    {
+        Console.Write("ISSO AI NÃO É UM NUMERO!");
+    }
+} while (entrada.ToUpper() != "OK");
