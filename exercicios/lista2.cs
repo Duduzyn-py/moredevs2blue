16615a807b65b78f28e55e41f961e4e4d9880646
Eduardo Caitano <83654560+Duduzyn-py@users.noreply.github.com>
2024-10-02 19:46:53 -0300

    Update lista2.cs

--- exercicios/lista2.cs
+++ exercicios/lista2.cs
@@ -28,3 +28,34 @@ else if (idade > 60)
 else
 {
     Console.WriteLine("Você pode estrar.");
+//INSTRUÇÃO SWITCH
+//1. Escreva um programa que peça ao usuário um número de 1 a 7 e após isso,
+//retorne o dia da semana correspondente a esse número. Exemplo: 1 == “Domingo”. 7 == “Sábado”.
+
+Console.WriteLine("Digite um número de dia da semana(1 a 7): ");
+var diaSemana =(Console.ReadLine());
+
+switch (diaSemana)
+{
+    case "1":
+        Console.WriteLine("Domingo");
+        break;
+    case "2":
+        Console.WriteLine("Segunda");
+        break;
+    case "3":
+        Console.WriteLine("Terça");
+        break;
+    case "4":
+        Console.WriteLine("Quarta");
+        break;
+    case "5":
+        Console.WriteLine("Quinta");
+        break;
+    case "6":
+        Console.WriteLine("Sexta");
+        break;
+    case "7":
+        Console.WriteLine("Sábado");
+        break;
+}
