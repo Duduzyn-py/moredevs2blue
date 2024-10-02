ffb51a875da2a01c190edfb8e2a7ea034301fd6e
Eduardo Caitano <83654560+Duduzyn-py@users.noreply.github.com>
2024-10-02 19:18:39 -0300

    Update lista2.cs

--- exercicios/lista2.cs
+++ exercicios/lista2.cs
@@ -1,1 +1,13 @@
+// INSTRUÇÃO IF
+//1. Escreva um programa que peça para o usuário um número. O programa deve
+//informar ao usuário se o número é ímpar ou par
+Console.WriteLine("Por informe um número");
+int num = int.Parse(Console.ReadLine());
 
+if (num % 2 == 0)
+{
+    Console.WriteLine($"{num} é par");
+}
+else { 
+    Console.WriteLine($"{num} é ímpar");
+}
