8ba6b5a9f23ff9544492518c8d8acb37d2a870d2
Eduardo Caitano <83654560+Duduzyn-py@users.noreply.github.com>
2024-10-02 19:26:33 -0300

    Update lista2.cs

--- exercicios/lista2.cs
+++ exercicios/lista2.cs
@@ -11,3 +11,20 @@ if (num % 2 == 0)
 else { 
     Console.WriteLine($"{num} é ímpar");
 }
+//2. Escreva um programa que peça ao usuário o nome e a idade.
+//Caso o usuário seja menor de idade ou sua idade seja superior a 60 anos,
+//o programa deve indicar que sua entrada será barrada por normas de segurança.
+
+Console.WriteLine("Bem vindo, por questões de segurança, informe sua idade por favor: ");
+int idade = int.Parse(Console.ReadLine());
+
+if (idade < 18) {
+    Console.WriteLine("Você é menor de idade, infelizmente não poderá entrar");
+}
+else if (idade > 60)
+{
+    Console.WriteLine("Você é hexagenário, ou superior em idade, infelizmente não poderá entrar");
+}
+else
+{
+    Console.WriteLine("Você pode estrar.");
